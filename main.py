3d53699b076e8cf73a13e4b1f0b08e4d142ac30c
jaybenaim <benaimjacob@gmail.com>
2019-08-21 11:10:00 -0400

    adds longest words if greater than last word

--- main.py
+++ main.py
@@ -2,14 +2,16 @@
 def longestConsecutive(strings, k): 
     # s = strings[0].split(' ') 
     strings_list = [] 
-    for string_index, string in enumerate(strings): 
-        # if len(string) > len(string[string_index]): 
-        strings_list.append(string)
-        
-    for s in strings_list: 
-        return s + s
-    # for string in strings: 
+  
+    for i in range(len(strings)): 
+        if len(strings[i]) > len(strings[i-1]):
+            strings_list.append(strings[i])
+        # print(strings_list)
 
+    for s in range(len(strings_list)): 
+        for j in range(k):
+            return  strings_list[j] + strings_list[j+1] + strings_list[j+2]
+        
 print(longestConsecutive(['hi', 'marbles', 'mittens', 'bye', 'lorem', 'ipsum', 'to', 'a', 'hippocampus'], 3) )
-# longestConsecutive(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2)
+print(longestConsecutive(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2))
 
