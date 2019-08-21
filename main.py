f12d8bfb3b3912b459b75bbd576d71aca7879893
jaybenaim <benaimjacob@gmail.com>
2019-08-21 17:32:25 -0400

    got right amount of words but wrong words

--- main.py
+++ main.py
@@ -4,12 +4,11 @@ def longestConsecutive(strings, k):
     strings_list = [] 
     new_list = [] 
     for i in range(len(strings)): 
-        if len(strings[i]) > len(strings[i+1]):
-
-            strings_list.append(strings[i])
+        if len(strings[i]) > len(strings[i-1]):
+            strings_list.append(strings[i] * k) 
     for s in range(k): 
 
-        return  strings_list[s] + strings_list[s+1] + strings_list[s+2]
+        return  strings_list[s] 
             
         
 print(longestConsecutive(['hi', 'marbles', 'mittens', 'bye', 'lorem', 'ipsum', 'to', 'a', 'hippocampus'], 3) )
