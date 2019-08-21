7a9d2f2050a8392e0d63592dbb7b7bb8a00aa0b4
jaybenaim <benaimjacob@gmail.com>
2019-08-21 19:09:58 -0400

    reinforcements complete

--- main.py
+++ main.py
@@ -3,13 +3,25 @@ def longestConsecutive(strings, k):
     # s = strings[0].split(' ') 
     strings_list = [] 
     new_list = [] 
+    
     for i in range(len(strings)): 
-        if len(strings[i]) > len(strings[i-1]):
-            strings_list.append(strings[i] * k) 
-    for s in range(k): 
-
-        return  strings_list[s] 
+        if len(strings[i] ) > len(strings[i-1]):
+            for s in range(k): 
+                strings_list.append(strings[i+s]) 
             
-        
-print(longestConsecutive(['hi', 'marbles', 'mittens', 'bye', 'lorem', 'ipsum', 'to', 'a', 'hippocampus'], 3) )
+            seperator = ''
+
+            return  seperator.join(strings_list)
+
+
+
+
+long1 = longestConsecutive(['hi', 'marbles', 'mittens', 'bye', 'lorem', 'ipsum', 'to', 'a', 'hippocampus'], 3) 
+print(long1)
 print(longestConsecutive(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2))
+
+# print(seperator.join(long1))
+
+
+
+
