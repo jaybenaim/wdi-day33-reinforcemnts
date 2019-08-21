98af0f80cbadc2ad320ab9119f23cf9ecf58fc22
jaybenaim <benaimjacob@gmail.com>
2019-08-21 17:27:17 -0400

    returning same number of words but

--- main.py
+++ main.py
@@ -2,16 +2,15 @@
 def longestConsecutive(strings, k): 
     # s = strings[0].split(' ') 
     strings_list = [] 
-  
+    new_list = [] 
     for i in range(len(strings)): 
         if len(strings[i]) > len(strings[i-1]):
-            strings_list.append(strings[i])
-        # print(strings_list)
 
-    for s in range(len(strings_list)): 
-        for j in range(k):
-            return  strings_list[j] + strings_list[j+1] + strings_list[j+2]
+            strings_list.append(strings[i])
+    for s in range(k): 
+
+        return  strings_list[s] + strings_list[s+1] + strings_list[s+2]
+            
         
 print(longestConsecutive(['hi', 'marbles', 'mittens', 'bye', 'lorem', 'ipsum', 'to', 'a', 'hippocampus'], 3) )
 print(longestConsecutive(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2))
-
