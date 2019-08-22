a8325fa6f44e5a6b86a0e69dd73d8d4b3fd3e433
jaybenaim <benaimjacob@gmail.com>
2019-08-21 20:09:37 -0400

    added longestInRow function to check the longest words by and display the amount of words passed in and got the longestConsecutive working corectly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 
 def longestConsecutive(strings, k): 
-    # s = strings[0].split(' ') 
     strings_list = [] 
     new_list = [] 
     
@@ -15,13 +14,27 @@ def longestConsecutive(strings, k):
 
 
 
-
 long1 = longestConsecutive(['hi', 'marbles', 'mittens', 'bye', 'lorem', 'ipsum', 'to', 'a', 'hippocampus'], 3) 
 print(long1)
 print(longestConsecutive(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2))
 
-# print(seperator.join(long1))
 
+def longestInRow(strings, k): 
+    items = []
+    new_list = [] 
+    for list_item_index, list_item in enumerate(strings):
+        if list_item not in items: 
+            items.append(list_item)
 
+    items.sort(key=len, reverse=True) 
+    new_list = items[:k]
+    new_list = list(new_list)
+    new_list = ''.join(new_list)
 
+    return new_list
+
+     
+  
+print(longestInRow(['hi', 'marbles', 'mittens', 'bye', 'lorem', 'ipsum', 'to', 'a', 'hippocampus'], 3))
+print(longestInRow(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2))
 
